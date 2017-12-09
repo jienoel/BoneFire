79032920a6ed86f0caabff10e871f8e693d332e2
jienoel <chenjienoel@163.com>
2017-12-09 10:55:26 +0800

    加入变换颜色

---
+++ Assets/Scripts/ConstUtil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class ColorTable
+{
+    public const int Red = 0;
+    public const int Green = 1;
+    public const int Max = 2;
+
+    public static bool isColorValid(int color)
+    {
+        return color < Max;
+    }
+}
+
+public class BodyID
+{
+    public const int Zero = 0;
+    public const int One = 1;
+    public const int Two = 2;
+    public const int Three = 3;
+    public const int Four = 4;
+}
+
+
+public class AnimatorParam
+{
+    public const string Color = "Color";
+}
---
+++ Assets/Scripts/Monster/MonsterBody.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBody : MonoBehaviour
+{
+    public int BodyID;
+    public Animator animator;
+    SpriteRenderer render;
+
+    void Awake()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        if (render == null)
+        {
+            render = GetComponent<SpriteRenderer>();
+        }
+//        animator.enabled = false;
+//        render.sprite = null;
+//        render.enabled = false;
+    }
+
+    public void EnableBody(int colorID)
+    {
+        render.enabled = true;
+        animator.enabled = true;
+        ChangeColor(colorID);
+    }
+
+    public void ChangeColor(int colorID)
+    {
+        if (ColorTable.isColorValid(colorID))
+            animator.SetFloat(AnimatorParam.Color, colorID);
+    }
+
+    public  bool changeColor1;
+    public bool changeColor2;
+    public int color1;
+    public  int color2;
+
+    void Update()
+    {
+        if (changeColor1)
+        {
+            color1 = (color1 + 1) % ColorTable.Max;
+            ChangeColor(color1);
+            changeColor1 = false;
+        }
+
+        if (changeColor2)
+        {
+            color2 = (color2 + 1) % ColorTable.Max;
+            render.color = color2 == ColorTable.Red ? Color.red : Color.green;
+            changeColor2 = false;
+        }
+    }
+
+}
